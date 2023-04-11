0293ef0c3eb8b08879659572b03e08f7cf74bf00
Rhys Betancourth <rdbrhys1@gmail.com>
2023-04-11 12:04:19 +0100

    checks to see if parentheses are closed and valid

--- validParentheses.py
+++ validParentheses.py
@@ -1,30 +1,23 @@
 class Solution(object):
     def isValid(self, s):
+        #gen stack
         stack = []
-
+        #runs through string
         for bra in s:
+            #if opening
             if bra in "([{":
+                #add to stack
                 stack.append(bra)
+            #if close
             else:
+                #if stack empty or close has no proper closer
                 if not stack or \
                 (bra == ")" and stack[-1] != "(") or \
                 (bra == "]" and stack[-1] != "[") or \
                 (bra == "}" and stack[-1] != "{"):
+                    #return false
                     return False
+                #handled so pop
                 stack.pop()
+        #if all stack empty after checks then not stack == True
         return not stack
-
-
-sol = Solution()
-
-print(sol.isValid("{[]}"))
-
-# s = "[{()}]"
-# a = [1, 5, 2, 6, 2]
-
-# for i in range((len(s) + 1) // 2):
-#     if s[i] == s[-1-i]:
-#         #print(s)
-#         s.replace(s[i], "")
-        
-# print(a[-1])
