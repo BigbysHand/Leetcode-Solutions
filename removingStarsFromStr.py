d4f0b8eb8bf6debce388d7daa8076e39cbd06f8e
Rhys Betancourth <rdbrhys1@gmail.com>
2023-04-12 14:14:31 +0100

    removes characters left of stars

--- removingStarsFromStr.py
+++ removingStarsFromStr.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def removeStars(self, s):
+        #out arr
         output = []
-        for i in range(len(s)):
-            if s[i] == "*":
+        #for char in s
+        for i in s:
+            #if i is a star pop last append
+            if i == "*":
                 output.pop()
+            #else app i
             else:
-                output += s[i]
+                output.append(i)
+        #ret str of output
         return "".join(output)
-    
-
-sol = Solution()
-
-print(sol.removeStars("leet**cod*e"))
