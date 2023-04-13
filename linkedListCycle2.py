86074a5095063968b0ca3885a232347d74098261
Rhys Betancourth <rdbrhys1@gmail.com>
2023-04-13 13:52:00 +0100

    returns the node that starts the cycle in a linked list

---
+++ linkedListCycle2.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        #floyds cycle algorithm
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow = head
+                while slow != fast:
+                    slow, fast = slow.next, fast.next
+                return slow
+        return None
