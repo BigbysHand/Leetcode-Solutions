8ced7a0bf4b5a6d95c478153a0b5074d005d54a0
BigbysHand <rdbrhys1@gmail.com>
2023-08-30 15:09:49 -0300

    indecies of two sum

---
+++ twoSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #def blank hash
+        prevDiff = {}
+        for i, x in enumerate(nums):
+            #diff is target - curr number
+            diff = target - x
+            if diff in prevDiff:
+                #ret since if diff already in hash then diff index and current are two sum
+                return [prevDiff[diff], i]
+            #let hash -> number:index
+            prevDiff[x] = i
